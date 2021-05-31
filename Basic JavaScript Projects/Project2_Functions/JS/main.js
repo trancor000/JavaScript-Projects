6045d8edfb3d0f6a2cda1276c473dc5f1fe3314f
trancor000 <83535306+trancor000@users.noreply.github.com>
2021-05-30 20:38:41 -0700

    Updated main

--- Basic JavaScript Projects/Project2_Functions/JS/main.js
+++ Basic JavaScript Projects/Project2_Functions/JS/main.js
@@ -1,5 +1,5 @@
-function myFunction() {
-    var sentence = "I am learning";
-    sentence += " a lot from this book!";
-    document.getElementById("Concatenate").innerHTML = sentence;
+function myFunction() { // Name of function to be called
+    var sentence = "I am learning"; // Assignment to variable
+    sentence += " a lot from this book!"; // Concatenation
+    document.getElementById("Concatenate").innerHTML = sentence; // How the function will be called
 }
