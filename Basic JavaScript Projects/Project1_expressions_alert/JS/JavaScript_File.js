30b67051194ae0cf6dea41c621da319343afeb7a
trancor000 <83535306+trancor000@users.noreply.github.com>
2021-05-30 17:18:05 -0700

    Basic JS Projects

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/JavaScript_File.js
@@ -0,0 +1,6 @@
+window.alert("Welcome to \"the\" show!"); // This will appear in the tab alert
+var A = "This is my string" // Assigning a string to a variable
+var City = "Frisco", Second = "Denton", Third = "Prosper" // Assigning multiple strings to a variable
+var City = City.fontcolor("red"); // Changing the font color to red
+document.write(A + Second) // Concatenating the variables being called
+document.write(19 + 95);
