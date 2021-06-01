9ef8cfd02cf29bdd3ee3395d8e9edb12e6eeeaf7
trancor000 <83535306+trancor000@users.noreply.github.com>
2021-05-31 18:54:53 -0700

    Functions

--- Basic JavaScript Projects/Project2_Functions/JS/main.js
+++ Basic JavaScript Projects/Project2_Functions/JS/main.js
@@ -1,4 +1,4 @@
-function myFunction() { // Name of function to be called
+function myFunction() { // Name of function to be called in index
     var sentence = "I am learning"; // Assignment to variable
     sentence += " a lot from this book!"; // Concatenation
     document.getElementById("Concatenate").innerHTML = sentence; // How the function will be called
