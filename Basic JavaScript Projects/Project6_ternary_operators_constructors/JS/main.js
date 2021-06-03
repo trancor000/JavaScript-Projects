31cfd05b82ada2e730ef7529df9ac9ee2e88cbcd
trancor000 <83535306+trancor000@users.noreply.github.com>
2021-06-03 16:56:49 -0700

    Update

--- Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -13,6 +13,21 @@ function myFunction() {
         " manufactured in " + Erik.Vehicle_Year;
 }
 
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+    document.getElementById("New_and_This").innerHTML =
+        "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +
+        " manufactured in " + Erik.Vehicle_Year;
+}
+
 function count_Function() {
     document.getElementById("Counting").innerHTML = Count();
     function Count() {
