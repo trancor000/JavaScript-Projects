558115c45e68e7d32facd8881cee97eba66aac30
trancor000 <83535306+trancor000@users.noreply.github.com>
2021-06-04 10:31:05 -0700

    Update

--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/index.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/index.js
@@ -1,20 +1,26 @@
-var slideIndex = [1, 1];
-/* Class the members of each slideshow group with different CSS classes */
-var slideId = ["mySlides1", "mySlides2"]
-showSlides(1, 0);
-showSlides(1, 1);
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function plusSlides(n, no) {
-    showSlides(slideIndex[no] += n, no)
+function plusSlides(n) { // function to be called from html for slide transition
+    showSlides(slideIndex += n); // Allows transition to next or previous slide
 }
 
-function showSlides(n, no) {
+function currentSlide(n) { // function to be called from html for dots/circles
+    showSlides(slideIndex = n); // Allows for transition when clicking on dots/circles
+}
+
+function showSlides(n) {
     var i;
-    var x = document.getElementsByClassName(slideId[no]);
-    if (n > x.length) { slideIndex[no] = 1 }
-    if (n < 1) { slideIndex[no] = x.length }
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
+    var slides = document.getElementsByClassName("mySlides"); // controls variable from myslides class
+    var dots = document.getElementsByClassName("dot"); // controls variable from dot class
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; // Does not show more than one slide
     }
-    x[slideIndex[no] - 1].style.display = "block";
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", ""); // Darkens current dot with slide
+    }
+    slides[slideIndex - 1].style.display = "block"; // Only shows the current slide
+    dots[slideIndex - 1].className += " active"; // Always shows the dots
 }
